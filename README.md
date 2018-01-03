300267b556dadf99fbc34d592a5e4a9d785f4fdc
Eduardo Areco <areco.eduardo@gmail.com>
2018-01-03 13:41:11 -0300

    video 3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MEVN-CRUD
+
+Siguiendo tutorial "Stack MEVN - Vuejs, Nodejs, Express y Mongodb"
+https://www.youtube.com/watch?list=PLL0TiOXBeDagPCEujOAX4nYxgxiJaR4ec&v=F53M1sbCPlM
+
+Excelente material de estudio!
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const bodyParser =require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+const itemRoutes = require('./routes/item')
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mevn').then(() => console.log('conectado a db'))
@@ -10,7 +13,14 @@ mongoose.connect('mongodb://localhost/mevn').then(() => console.log('conectado a
 app.set('port', process.env.PORT || 3000);
 
 //middlewares
+app.use(cors());
 app.use(bodyParser.json());
+
+//routes
+app.use('/item' ,itemRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('servidor en el puerto', app.get('port'));
